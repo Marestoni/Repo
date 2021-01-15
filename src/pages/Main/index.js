90e2b344b9debef18615db0236d5073ec2c0c8f7
André Messias Marestoni <m.marestoni@gmail.com>
2021-01-15 13:26:14 -0300

    adicionando axios para consumo da api do github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import  React, {useState, useCallback}  from 'react';
 
-import {Title} from './styles';
+import {FaGithub,FaPlus} from 'react-icons/fa';
+import {Container, Form, SubmitButton} from './styles';
+
+
+import api from '../../services/api'
 
 export default function Main() {
-    return (
-        <Title>
-            Main
 
-        </Title>
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+
+    function handleinputChange(e){
+        setNewRepo(e.target.value);        
+    }
+
+    const handleSubmit = useCallback((e)=>{
+        e.preventDefault();
+        
+        async function submit(){
+        const response = await api.get(`repos/${newRepo}`);
+
+        const data = {
+            name: response.data.full_name,
+        }
+
+        setRepositorios([...repositorios, data])
+        setNewRepo('');
+        }
+        submit();
+    },[newRepo, repositorios]);
+
+
+    return (
+        <Container>
+             <h1> 
+                <FaGithub size={25} />
+                Meus Repositorios
+            </h1>
+            <Form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Adicionar Repositorios"
+                    value={newRepo}
+                    onChange={handleinputChange}
+                    />
+
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
     )
 }
